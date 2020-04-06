18b133d4957507966cf95a97e6b3da84ea4d91ea
Dmitriy Nakhimovich <Dmitriy.Nahimovich@artezio.com>
2020-04-06 15:54:28 +0300

    update package add ra fix

--- api/courses/controllers/courses.js
+++ api/courses/controllers/courses.js
@@ -1,8 +1,18 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  async find(ctx) {
+    let entities;
+    ctx.set('Content-Range', await strapi.services.post.count());
+    if (ctx.query._q) {
+      entities = await strapi.services.post.search(ctx.query);
+    } else {
+      entities = await strapi.services.post.find(ctx.query);
+    }
 
-module.exports = {};
+    return entities.map(entity =>
+      sanitizeEntity(entity, { model: strapi.models.post })
+    );
+  },
+};
