4b170560ca4c795a6ee71cdb506f97133c3a9106
Dmitriy Nakhimovich <Dmitriy.Nahimovich@artezio.com>
2020-04-08 12:36:17 +0300

    fix courses

--- api/courses/controllers/courses.js
+++ api/courses/controllers/courses.js
@@ -4,15 +4,15 @@ const { sanitizeEntity } = require('strapi-utils');
 module.exports = {
   async find(ctx) {
     let entities;
-    ctx.set('Content-Range', await strapi.services.post.count());
+    ctx.set('Content-Range', await strapi.services.courses.count());
     if (ctx.query._q) {
-      entities = await strapi.services.post.search(ctx.query);
+      entities = await strapi.services.courses.search(ctx.query);
     } else {
-      entities = await strapi.services.post.find(ctx.query);
+      entities = await strapi.services.courses.find(ctx.query);
     }
 
     return entities.map(entity =>
-      sanitizeEntity(entity, { model: strapi.models.post })
+      sanitizeEntity(entity, { model: strapi.models.courses })
     );
   },
 };
